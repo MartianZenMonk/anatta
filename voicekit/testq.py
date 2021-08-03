3293cc44cb4a3e081547e34cdadebd114ce3fd4b
Martian Zen Monk <dropoutunit@gmail.com>
2021-08-03 10:07:35 +0700

    add motion detect

--- voicekit/testq.py
+++ voicekit/testq.py
@@ -17,6 +17,7 @@ import time
 import csv
 from subprocess import call
 import socket
+import cv2
 
 import datetime as dt
 from datetime import datetime
@@ -153,6 +154,65 @@ def get_ip():
     return IP
 
 #TEST
+def motion_detect():
+    bk = False
+    # Assigning our static_back to None
+    static_back = None
+    # List when any moving object appear
+    motion_list = [ None, None ]
+    video = cv2.VideoCapture(0)
+    while True:
+        # Reading frame(image) from video
+        check, frame = video.read()
+        # Initializing motion = 0(no motion)
+        motion = 0
+
+        # Converting color image to gray_scale image
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Converting gray scale image to GaussianBlur
+        # so that change can be find easily
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+        # In first iteration we assign the value
+        # of static_back to our first frame
+        if static_back is None:
+            static_back = gray
+            continue
+
+        # Difference between static background
+        # and current frame(which is GaussianBlur)
+        diff_frame = cv2.absdiff(static_back, gray)
+
+        # If change in between static background and
+        # current frame is greater than 30 it will show white color(255)
+        thresh_frame = cv2.threshold(diff_frame, 30, 255, cv2.THRESH_BINARY)[1]
+        thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)
+
+        # Finding contour of moving object
+        _,cnts,_ = cv2.findContours(thresh_frame.copy(),
+                        cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in cnts:
+            if cv2.contourArea(contour) < 10000:
+                continue
+            motion = 1
+
+            (x, y, w, h) = cv2.boundingRect(contour)
+            # print(str(w*h))
+            # making green rectangle arround the moving object
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            if w*h > 300000:
+                bk = True
+
+        # Appending status of motion
+        motion_list.append(motion)
+
+        if bk:
+            break
+    return None
+
+
 def killPlayer():
     if find_name('mpg123'):
         os.system("killall mpg123")
@@ -320,7 +380,7 @@ try:
             v =  '["please zen story lord buddha buddhist buddhism what time day play help dhamma meditation english radio start light '
             v += 'browse chanting mantra say speak stop volume turn on off exit shutdown now thai lyric ip address sutra up down breathing '
             v += 'one two three four five six seven eight nine ten zero fiftheen twenty thirty fourty fifthy sixty red green blue yellow '
-            v += 'yes no ok coca cola stage fold path nature truth dependent origination webcam"]'
+            v += 'yes no ok coca cola stage fold path nature truth dependent origination webcam loop"]'
 
             rec = vosk.KaldiRecognizer(model, args.samplerate,v)
 
@@ -371,16 +431,20 @@ try:
                                     zen = True
 
                                 elif "breathing" in words and "chanting" in words:
-                                    killPlayer()   
-                                    speak("Thai Anapanasati chanting")
-                                    proc = subprocess.Popen(["mpg123","-f","2000","-C","--loop","-1","../datath/chanting/anapanasati-cut.mp3"], stdin=master)
-                                    press_for_stop()
+                                    killPlayer()
+                                    if "loop" in words:
+                                        speak("Thai Anapanasati chanting")
+                                        proc = subprocess.Popen(["mpg123","-f","2000","-C","--loop","-1","../datath/chanting/anapanasati-cut.mp3"], stdin=master)
+                                        press_for_stop()
+                                    else:
+                                        subprocess.run(["mpg123","-f","2000","../datath/chanting/anapanasati-cut.mp3"])
 
                                 elif "nature" in words and "truth" in words and "chanting" in words:
                                     killPlayer()
                                     speak("Thai Dhamma Ni yam chanting")
                                     proc = subprocess.Popen(["mpg123","-f","2000","-C","--loop","-1","../datath/chanting/dhammaniyam.mp3"], stdin=master)
-                                    press_for_stop()
+                                    motion_detect()
+                                    proc.kill()
 
                                 elif "dependent" in words and "origination" in words and "chanting" in words:
                                     killPlayer()  
@@ -483,14 +547,14 @@ try:
                                 elif "play" in words and "mantra" in words and "six" in words:
                                     killPlayer()    
                                     speak("4 hours buddho mantra then shutdown")
-                                    leds.update(Leds.rgb_on(Color.GREEN)) 
+                                    leds.update(Leds.rgb_on(Color.YELLOW)) 
                                     proc = subprocess.Popen(["mpg123","-d","3","-f","1500","-q","--loop","-1","../thaivoices/buddho.mp3"])
                                     time.sleep(1800)
                                     proc.kill()
                                     speak("Do not forget to mind your breathing, mind your body movement and mind your mind.")
                                     text = " ../thaivoices/sati.mp3"
                                     os.system("mpg123 -q -f 2000 "+text)
-                                    leds.update(Leds.rgb_on(Color.YELLOW)) 
+                                    leds.update(Leds.rgb_on(Color.GREEN)) 
                                     proc = subprocess.Popen(["mpg123","-d","3","-f","1500","-q","--loop","-1","../thaivoices/buddho.mp3"])
                                     time.sleep(1800)
                                     proc.kill()
